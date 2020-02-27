7e53fbb0d73efbb649172c5e130b9e3aef111aa3
sony <sony@localhost.localdomain>
2020-02-27 14:18:40 +0000

    base

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,12 +3,14 @@ import firebase from "firebase/app";
 import "./App.css";
 import { Typography, Paper, Button } from "@material-ui/core";
 import logo from "../assets/cactus.png";
+import Iframe from "react-iframe";
 
 const db = () => firebase.database();
 
 function Dashboard() {
   const [reservas, handleReservas] = useState([]);
   const [input, handleInput] = useState("");
+  const [item, setItem] = useState("");
 
   useEffect(() => {
     db();
@@ -59,11 +61,14 @@ function Dashboard() {
       .replace("userInfo", "")
       .replace(":", "")
   );
+  const product = "https://www.milanuncios.com/renovar/?id=" + myDashboard[0];
   return (
     <div>
       <p>{myDashboard[0]}</p>
       <p>{myDashboard[1]}</p>
       <button onClick={() => logOut()}>Log Out</button>
+      <Iframe url="https://www.milanuncios.com"></Iframe>
+      <Iframe url={product}></Iframe>
     </div>
   );
 }
