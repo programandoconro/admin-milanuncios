f90c142125748a5c102ab20674e4a606fd80e714
root <root@localhost.localdomain>
2020-03-07 22:12:32 +0000

    Updates

---
+++ precache-manifest.255be1321cc845c6a2b68c80729fd4b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbdbfed8dd74b64d8a887047f271925",
+    "url": "/admin'milanuncios/index.html"
+  },
+  {
+    "revision": "ca0175bdcc4cdb3d71a1",
+    "url": "/admin'milanuncios/static/css/main.7f6894d8.chunk.css"
+  },
+  {
+    "revision": "23a04d8c395932c5a96d",
+    "url": "/admin'milanuncios/static/js/2.f81a67dc.chunk.js"
+  },
+  {
+    "revision": "252ea9a7224f0456cd564305a57c0bec",
+    "url": "/admin'milanuncios/static/js/2.f81a67dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca0175bdcc4cdb3d71a1",
+    "url": "/admin'milanuncios/static/js/main.27cce729.chunk.js"
+  },
+  {
+    "revision": "ed045c28054348f41b4e",
+    "url": "/admin'milanuncios/static/js/runtime-main.c2868cd2.js"
+  }
+]);
